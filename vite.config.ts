78a295542c696ce8b785cc9a1a1a0d8db15cb61c
greeneyz <asim.ali.awan@gmail.com>
2025-08-26 15:50:45 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -20,4 +20,8 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: './', // <- add this line to fix GitHub Pages 404s
+  },
 }));
