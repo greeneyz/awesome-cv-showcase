be2c728b3c3aece4c0fd630c37ca79f0e0e214c6
greeneyz <asim.ali.awan@gmail.com>
2025-08-26 14:33:06 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/',
+  base: '/awesome-cv-showcase/',
   server: {
     host: "::",
     port: 8080,
