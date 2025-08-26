0757ccf3ec9764dc63cb8d05461504785c0d9e29
greeneyz <asim.ali.awan@gmail.com>
2025-08-26 16:44:11 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
+  base: "/awesome-cv-showcase/",
   plugins: [react()],
-  base: '/awesome-cv-showcase/', // 👈 repo name
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: "dist",
+  },
+});
